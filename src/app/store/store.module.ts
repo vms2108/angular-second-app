09dc11bceacfc7d3231c4c07af20480c19abb803
Mikhail <vms2108@mail.ru>
2018-07-14 11:18:20 +0300

    добавление корзины товаров

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import {ModelModule} from '../model/model.module';
 import { StoreComponent } from './store.component';
 import {FormsModule} from '@angular/forms';
+import { CartSummaryComponent } from './cartSummary.component';
 @NgModule({
     imports: [ModelModule, BrowserModule, FormsModule],
-    declarations: [StoreComponent],
+    declarations: [StoreComponent, CartSummaryComponent],
     exports: [StoreComponent]
 })
 export class StoreModule {}
